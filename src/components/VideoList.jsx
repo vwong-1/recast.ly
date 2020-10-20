45be4e80c39ce885fdce3a0d6a8ebfbef789d902
Victor Wong <victorw1992@gmail.com>
2020-10-19 22:32:40 -0700

    Brought back the specs in SpecRunner

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,4 +1,4 @@
-import VideoListEntry from './VideoListEntry';
+import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => (
   <div className="video-list">
@@ -17,15 +17,3 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
-
-/*
-var VideoList = (props) => (
-  <div className="video-list">
-  <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  <div><h5><em>videoListEntry</em> view goes here</h5></div>
-</div>
-);
-*/
